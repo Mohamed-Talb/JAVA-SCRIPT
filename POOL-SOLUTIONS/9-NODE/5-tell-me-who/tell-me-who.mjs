10d2f18e85ca32c00e33f9772eeacd66f9a8cd54
Mohamed Taleb <mtaleb.contact@gmail.com>
2026-03-22 17:36:29 +0100

    Add 10-NODE/ex01-ex02-ex03-ex04-ex05-ex06 after testing validation

--- POOL-SOLUTIONS/9-NODE/5-tell-me-who/tell-me-who.mjs
+++ POOL-SOLUTIONS/9-NODE/5-tell-me-who/tell-me-who.mjs
@@ -2,14 +2,31 @@ import fs from 'fs'
 
 function getFullname(JsonFilename)
 {
-	let fullName 
-	JsonFilename.split(".")[0];
+	let fullName = JsonFilename.split(".")[0].split("_");
+	return `${fullName[1]} ${fullName[0]}`;
 }
 
+function compareTwoStrings(a, b)
+{
+	const lowerA = a.toLowerCase();
+	const lowerB = b.toLowerCase();
+	if (lowerA < lowerB)
+		return -1;
+	else if (lowerA > lowerB)
+		return 1;
+	else 
+		return 0;
+}
+
+function printList(List)
+{
+	for (let i in List)
+		console.log(`${Number(i)+1}. ${List[i]}`);
+}
 
 let path = process.argv[2];
 if (path === undefined)
 	path = './';
 let namesList = fs.readdirSync(path).map(getFullname);
-
-console.log(content);
+namesList = namesList.sort(compareTwoStrings);
+printList(namesList);
